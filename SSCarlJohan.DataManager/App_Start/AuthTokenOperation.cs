525791a7d0b1acf34d5af4671a9ce6efdf584edc
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-09-06 21:59:24 +0200

    Create AuthTokenOperation.cs

---
+++ SSCarlJohan.DataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,17 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace SSCarlJohan.DataManager.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
