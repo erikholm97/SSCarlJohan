deb7715e8ef9ceef3e54bbb5dd598eb2b556c0c1
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-09-12 22:11:50 +0200

    Squashed commit of the following:
    commit 137184e1cb89c4af7d7fe59c3e7dc1e712fb8fb5
    Author: Erik Holm <54987631+erikholm97@users.noreply.github.com>
    Date:   Sun Sep 12 22:04:21 2021 +0200
    
        Added format for password parameter in AuthTokenOperation
    
        When typing password text is hidden
    
    commit be4cfdb0dc44b7605700868f200222c80864b170
    Author: Erik Holm <54987631+erikholm97@users.noreply.github.com>
    Date:   Sun Sep 12 21:54:15 2021 +0200
    
        Added Document filter for AuthTokenOperation
    
    commit 1f0cebb6f8d7050129f8c5d96e6b8f9ab2ed0371
    Author: Erik Holm <54987631+erikholm97@users.noreply.github.com>
    Date:   Sun Sep 12 21:46:16 2021 +0200
    
        Added auth token Operation

--- SSCarlJohan.DataManager/App_Start/AuthTokenOperation.cs
+++ SSCarlJohan.DataManager/App_Start/AuthTokenOperation.cs
@@ -11,7 +11,43 @@ namespace SSCarlJohan.DataManager.App_Start
     {
         public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
         {
-            throw new NotImplementedException();
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = true,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = true,
+                            @in = "formData",
+                            format = "password"
+                        },
+                    }
+                }
+            });
         }
     }
 }
