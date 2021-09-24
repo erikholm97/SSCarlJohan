3979e72a7fd360703c87885817f0fdc374efdfbf
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-09-24 23:06:00 +0200

    Squashed commit of the following:
    commit 42a1387b8a4122f3b236a1555ef16aba961bebff
    Author: Erik Holm <54987631+erikholm97@users.noreply.github.com>
    Date:   Fri Sep 24 23:05:45 2021 +0200
    
        Added Wpf project

---
+++ SSCarlJohanDesktop.UI/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using SSCarlJohanDesktop.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SSCarlJohanDesktop.UI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
