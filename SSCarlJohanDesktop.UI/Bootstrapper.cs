c7af5f34ab929ee93bc6d73a4396ca0d3218bf7e
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-09-27 21:50:14 +0200

    Update Bootstrapper.cs

--- SSCarlJohanDesktop.UI/Bootstrapper.cs
+++ SSCarlJohanDesktop.UI/Bootstrapper.cs
@@ -11,14 +11,35 @@ namespace SSCarlJohanDesktop.UI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            base.BuildUp(instance);
         }
     }
 }
