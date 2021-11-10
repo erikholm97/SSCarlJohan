76ec0fc72b130f2da25eda698ffb58ef917af741
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-11-10 21:47:43 +0100

    Create APIHelper.cs

---
+++ SSCarlJohanDesktop.UI/Helpers/APIHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSCarlJohanDesktop.UI.Helpers
+{
+    public class APIHelper
+    {
+        public HttpClient ApiClient { get; set; }
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            string apiUri = ConfigurationManager.AppSettings["api"];
+
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri(apiUri);
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task AuthenticateAsync(string username, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("grant_type", password),
+            });
+
+            using(HttpResponseMessage response = await ApiClient.PostAsync("/Token", data))
+            {
+
+            }
+
+        }
+    }
+}
