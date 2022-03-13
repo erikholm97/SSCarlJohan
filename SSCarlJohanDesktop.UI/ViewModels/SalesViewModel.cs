13560056897fc99ed8311e31d7d23659d3b0826d
Erik Holm <erikholm97@hotmail.com>
2022-03-13 19:06:35 +0100

    Added the ticket sales page and supporting viewmodel
    Add sales page
    
    Adds the sales page of Ticket page XAML and the supporting viewmodel.

---
+++ SSCarlJohanDesktop.UI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,131 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSCarlJohanDesktop.UI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set 
+            {                
+                _products = value; 
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        private int _itemQuantity;
+
+        public int ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set 
+            { 
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                return "0.00 KR";
+            }
+        }
+
+        public string Tax
+        {
+            get
+            {
+                return "0.00 KR";
+            }
+        }
+
+        public string Total
+        {
+            get
+            {
+                return "0.00 KR";
+            }
+        }
+
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+
+                //Make sure something is selected
+                // Make sure there is an item quantity.
+
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+        
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+
+                //Make sure something is selected
+                // Make sure there is an item quantity.
+
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+
+                //Make sure something is selected
+                // Make sure there is an item quantity.
+
+                return output;
+            }
+        }
+
+        public void CheckOut()
+        {
+
+        }
+    }
+}
