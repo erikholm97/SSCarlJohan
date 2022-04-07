475b3375e155b429c020272b6e558ca0077309db
Erik Holm <erikholm97@hotmail.com>
2022-04-07 21:43:12 +0200

    Addition of Product table
    Added classes and code for returning product information through the api.
    
    Table modification to Inventory and Product.
    
    Added Get Products which returns all products in the database through an stored procedure.

---
+++ SSCarlJohan.DataManager.Library/DataAccess/ProductData.cs
@@ -0,0 +1,18 @@
+﻿using SSCarlJohan.DataManager.Library.Internal.DataAccess;
+using SSCarlJohan.DataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace SSCarlJohan.DataManager.Library.DataAccess
+{
+    public class ProductData
+    {
+        public List<ProductModel> GetProducts()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetAll", new { }, "SSCarlJohanConnection");
+
+            return output;
+        }
+    }
+}
