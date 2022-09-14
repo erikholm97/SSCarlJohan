a70c806a3d589b5c6af7fb4e7308efa7b23e4731
Erik Holm <erikholm97@hotmail.com>
2022-09-14 07:04:19 +0200

    Added log of method

--- SSCarlJohanDesktop.UI/ViewModels/ShellViewModel.cs
+++ SSCarlJohanDesktop.UI/ViewModels/ShellViewModel.cs
@@ -1,20 +1,41 @@
 ﻿using Caliburn.Micro;
+using SSCarlJohan.Desktop.UI.Library.Models;
 using SSCarlJohanDesktop.UI.EventModels;
 
 namespace SSCarlJohanDesktop.UI.ViewModels
 {
     public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
-    {        
+    {
         private IEventAggregator _events;
-        private SalesViewModel _salesVM;        
+        private SalesViewModel _salesVM;
+        private ILoggedInUserModel _user;
 
-        public ShellViewModel(SalesViewModel salesVM, IEventAggregator events)
+        public ShellViewModel(SalesViewModel salesVM,
+            IEventAggregator events,
+            ILoggedInUserModel user)
         {
-            _events = events;                        
-            _salesVM = salesVM;            
+            _events = events;
+            _salesVM = salesVM;
+            _user = user;
             _events.Subscribe(this);
-            
+
             ActivateItem(IoC.Get<LoginViewModel>());
+        }
+
+        public bool IsAccountVisible
+        {
+            get 
+            { 
+                bool output = false;
+
+                if(string.IsNullOrWhiteSpace(_user.Token) == false)
+                {
+                    output = true;
+                }
+                
+                return output;
+            
+            }
         }
 
         public void ExitApplication()
@@ -24,11 +45,13 @@ namespace SSCarlJohanDesktop.UI.ViewModels
 
         public void LogOut()
         {
-
+            _user.LogOffUser();
         }
 
         public void Handle(LogOnEvent message)
-            => ActivateItem(_salesVM);            
-        
+        {
+            ActivateItem(_salesVM);
+            NotifyOfPropertyChange(() => IsAccountVisible);
+        }            
     }
 }
