9b8f6e47e7a14af6d13f2c1a8ecbdf741f302fe0
Erik Holm <erikholm97@hotmail.com>
2022-10-11 19:50:40 +0200

    Add dialog box and check permission
    Checks to make sure that user is authorized to view the salespage.

--- SSCarlJohanDesktop.UI/ViewModels/SalesViewModel.cs
+++ SSCarlJohanDesktop.UI/ViewModels/SalesViewModel.cs
@@ -7,9 +7,11 @@ using SSCarlJohanDesktop.UI.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SSCarlJohanDesktop.UI.ViewModels
 {
@@ -19,21 +21,50 @@ namespace SSCarlJohanDesktop.UI.ViewModels
         ISaleEndPoint _saleEndPoint;
         IConfigHelper _configHelper;
         IMapper _mapper;
+        private readonly StatusInfoViewModel _status;
+        private readonly IWindowManager _window;
 
         public SalesViewModel(IProductEndPoint productEndPoint,
             IConfigHelper configHelper,
             ISaleEndPoint saleEndPoint,
-            IMapper mapper)
+            IMapper mapper,
+            StatusInfoViewModel status,
+            IWindowManager window)
         {
             _productEndPoint = productEndPoint;
             _configHelper = configHelper;
             _saleEndPoint = saleEndPoint;
             _mapper = mapper;
+            this._status = status;
+            this._window = window;
         }        
         protected override async void OnViewLoaded(object view)
         {
             base.OnViewLoaded(view);
-            await LoadProducts();
+            try
+            {
+                await LoadProducts();
+            }
+            catch (Exception ex)
+            {
+                dynamic settings = new ExpandoObject();
+                settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                settings.ResizeMode = ResizeMode.NoResize;
+                settings.Title = "System Error";
+
+                if(ex.Message == "Unauthorized")
+                {
+                    _status.UpdateMessage("Unauthorized Access", "You do not have permission to interact with the Sales Form");
+                    _window.ShowDialog(_status, null, settings);
+                }
+                else
+                {
+                    _status.UpdateMessage("Fatal Exception", ex.Message);
+                    _window.ShowDialog(_status, null, settings);
+                }
+                
+                TryClose();
+            }
         }
 
         private async Task LoadProducts()
