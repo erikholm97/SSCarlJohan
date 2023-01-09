7f3b51b557e55ae7d84351ba85123745ad1c8a13
Erik Holm <erikholm97@hotmail.com>
2023-01-09 21:47:11 +0100

    Added code snippet for adding roles to standard account.

--- SSCarlJohan.WebAPI/Pages/Privacy.cshtml.cs
+++ SSCarlJohan.WebAPI/Pages/Privacy.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,14 +12,40 @@ namespace SSCarlJohan.WebAPI.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public PrivacyModel(ILogger<PrivacyModel> logger, 
+            RoleManager<IdentityRole> roleManager
+            UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            string[] roles = new string[] { "Manager", "Cashier", "Admin" };
+
+            foreach (var role in roles)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+
+                if(roleExist is false) 
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            var user = await userManager.FindByEmailAsync("erikholm97@gmail.com");
+
+            if(user != null)
+            {
+                await userManager.AddToRoleAsync(user, "Admin");
+                await userManager.AddToRoleAsync(user, "Cashier");
+                await userManager.AddToRoleAsync(user, "Manager");
+            }
         }
     }
 }
