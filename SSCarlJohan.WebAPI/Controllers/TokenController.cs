7edc76be4b9ad37818255f53d68d7969eda3465d
Erik Holm <erikholm97@hotmail.com>
2023-01-18 07:01:34 +0100

    Added GenerateToken method with claimes and roles

--- SSCarlJohan.WebAPI/Controllers/TokenController.cs
+++ SSCarlJohan.WebAPI/Controllers/TokenController.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SSCarlJohan.WebAPI.Data;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.IdentityModel.JsonWebTokens;
+using System;
 
 namespace SSCarlJohan.WebAPI.Controllers
 {
@@ -10,44 +15,53 @@ namespace SSCarlJohan.WebAPI.Controllers
     [ApiController]
     public class TokenController : ControllerBase
     {
-        //private readonly ApplicationDbContext context;
-        //private readonly UserManager<IdentityUser> userManager;
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<IdentityUser> userManager;
 
-        //public TokenController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
-        //{
-        //    this.context = context;
-        //    this.userManager = userManager;
-        //}
+        public TokenController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
 
-        //[Route("/token")]
-        //[HttpPost]
-        //public async Task<IActionResult> Create(string username, string password, string grant_type)
-        //{
-        //    if(await IsValidUsernameAndPassword(username, password))
-        //    {
+        [Route("/token")]
+        [HttpPost]
+        public async Task<IActionResult> Create(string username, string password, string grant_type)
+        {
+            if (await IsValidUsernameAndPassword(username, password))
+            {
 
-        //    }
-        //    else
-        //    {
-        //        return BadRequest();
-        //    }
-        //}
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
 
-        //private async Task<bool> IsValidUsernameAndPassword(string username, string password)
-        //{
-        //    var user = await userManager.FindByEmailAsync(username);
+        private async Task<bool> IsValidUsernameAndPassword(string username, string password)
+        {
+            var user = await userManager.FindByEmailAsync(username);
 
-        //    return await userManager.CheckPasswordAsync(user, password);
-        //}
+            return await userManager.CheckPasswordAsync(user, password);
+        }
 
-        //private async Task<dynamic> GenerateToken(string username)
-        //{
-        //    //var user = await userManager.FindByEmailAsync(username);
+        private async Task<dynamic> GenerateToken(string username)
+        {
+            var user = await userManager.FindByEmailAsync(username);
 
-        //    //var roles = from ur in context.UserRoles
-        //    //            join r in context.Roles on ur.RoleId equals r.Id
-        //    //            where ur.UserId == user.Id
-        //    //            select new { ur.UserId, ur.RoleId, r.Name };
-        //}
+            var roles = from ur in context.UserRoles
+                        join r in context.Roles on ur.RoleId equals r.Id
+                        where ur.UserId == user.Id
+                        select new { ur.UserId, ur.RoleId, r.Name };
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(JwtRegisteredClaimNames.Nbf, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString())
+            };
+
+            return null;
+        }
     }
 }
