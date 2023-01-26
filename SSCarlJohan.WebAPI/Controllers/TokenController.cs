fe497b9b3fc80bf60bd4057a0b08eac2dc895b72
Erik Holm <erikholm97@hotmail.com>
2023-01-26 22:01:44 +0100

    Added FromForm tag to catch username and password in tokencontroller

--- SSCarlJohan.WebAPI/Controllers/TokenController.cs
+++ SSCarlJohan.WebAPI/Controllers/TokenController.cs
@@ -28,7 +28,7 @@ namespace SSCarlJohan.WebAPI.Controllers
 
         [Route("/token")]
         [HttpPost]
-        public async Task<IActionResult> Create(string username, string password, string grant_type)
+        public async Task<IActionResult> Create([FromForm] string username, [FromForm] string password, [FromForm] string grant_type)
         {
             if (await IsValidUsernameAndPassword(username, password))
             {
