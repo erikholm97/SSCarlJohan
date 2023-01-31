de82f6a61ce2360a3e2676c6bb179c623b93d007
Erik Holm <erikholm97@hotmail.com>
2023-01-31 06:58:32 +0100

    Updated UserController routes

--- SSCarlJohan.WebAPI/Controllers/UserController.cs
+++ SSCarlJohan.WebAPI/Controllers/UserController.cs
@@ -43,7 +43,7 @@ namespace SSCarlJohan.WebAPI.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpGet]
-        [Route("api/User/Admin/GetAllUsers")]
+        [Route("Admin/GetAllUsers")]
         public List<ApplicationUserModel> GetAllUsers()
         {
             List<ApplicationUserModel> output = new List<ApplicationUserModel>();
@@ -72,7 +72,7 @@ namespace SSCarlJohan.WebAPI.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpGet]
-        [Route("api/User/Admin/GetAllRoles")]
+        [Route("Admin/GetAllRoles")]
         public Dictionary<string, string> GetAllRoles()
         {
             var roles = _applicationDbContext.Roles.ToDictionary(x => x.Id, x => x.Name);
@@ -83,7 +83,7 @@ namespace SSCarlJohan.WebAPI.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpPost]
-        [Route("api/User/Admin/AddRole")]
+        [Route("Admin/AddRole")]
         public async Task AddRole(UserRolePairModel pairing)
         {
             var user = await _userManager.FindByIdAsync(pairing.UserId);
@@ -92,7 +92,7 @@ namespace SSCarlJohan.WebAPI.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpPost]
-        [Route("api/User/Admin/RemoveRole")]
+        [Route("Admin/RemoveRole")]
         public async Task RemoveRole(UserRolePairModel pairing)
         {
             var user = await _userManager.FindByIdAsync(pairing.UserId);
